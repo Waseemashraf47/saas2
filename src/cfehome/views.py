e26d5427c851993f0147583909db64f1b72732e1
waseem <waseemashraf47@gmail.com>
2025-01-14 15:54:48 +0500

    Protected view added which requires login

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 import pathlib
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from visits.models import PageVisit
 
 this_dir =  pathlib.Path(__file__).resolve().parent
@@ -57,3 +58,26 @@ def my_old_home_page_view(request, *args, **kwargs):
     # html_ = html_file_path.read_text()
     # return HttpResponse("<h1>Hello World</h1>")
     return HttpResponse(html_)
+
+VALID_CODE = "abc123"
+
+# def pw_protected_view(request, *args, **kwarg*):
+def pw_protected_view(request, *args, **kwargs):
+    is_allowed = request.session.get('protected_page_allowed') or 0
+    # print(request.session.get('protected_page_allowed'), type(request.session.get('protected_page_allowed')))
+    if request.method == "POST":
+        user_pw_sent = request.POST["code"] or None
+        if user_pw_sent == VALID_CODE:
+            is_allowed = 1
+            request.session['protected_page_allowed'] = is_allowed
+    if is_allowed:
+        return render(request, "protected/view.html", {})
+    return render(request, "protected/entry.html", {})
+
+
+@login_required
+def user_only_view(request, *args, **kwargs):
+    # if request.user.is_authentiated:
+    #     return redirect("/login")
+    
+    return render(request, "protected/user_only.html", {})
